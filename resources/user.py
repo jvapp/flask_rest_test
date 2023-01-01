2fc08829593134ade1c6e9d2bd2162ed728cb5a8
Joseph App <jvapp@Josephs-MacBook-Pro.local>
2023-01-01 12:02:42 -0600

    Send mail with mailgun.

--- resources/user.py
+++ resources/user.py
@@ -1,33 +1,60 @@
+import os
+import requests
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_jwt
-from blocklist import BLOCKLIST
+from sqlalchemy import or_
 
+from blocklist import BLOCKLIST
 from db import db
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+        f'https://api.mailgun.net/v3/{domain}/messages',
+        auth=("api", os.getenv("MAILGUN_API_KEY")),
+        data={"from": f"Data Admin <mailgun@{domain}>",
+              "to": [to],
+              "subject": [subject],
+              "text": body})
+
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
         # could do with IntegrityCheck
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+        if UserModel.query.filter(
+                or_(
+                    UserModel.username == user_data["username"],
+                    UserModel.email == user_data["email"]
+                )
+        ).first():
             abort(
                 409,
-                message="A user with that username already exists."
+                message="A user with that username or email already exists."
             )
+
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"]),
         )
 
         db.session.add(user)
         db.session.commit()
+
+        send_simple_message(
+            to=user.email,
+            subject="Successfully signed up",
+            body=f'Hi {user.username}! You have successfully signed up for the API.'
+        )
 
         return {"message": "User created successfully."}, 201
 
@@ -80,6 +107,3 @@ class User(MethodView):
         db.session.delete(user)
         db.session.commit()
         return {"message": "User deleted."}, 200
-
-
-
