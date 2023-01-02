c2be2bdc5955e9639a4da7dff9ce57a9944a9883
Joseph App <jvapp@Josephs-MacBook-Pro.local>
2023-01-02 15:46:00 -0600

    Add settings.py for rq worker. Add html template for email registration. Use rq worker for task queues.

--- tasks.py
+++ tasks.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import jinja2
+
 from dotenv import load_dotenv
 
 """
@@ -10,16 +12,25 @@ will not load env vars into this script. load_dotenv() must be called here.
 load_dotenv()
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
 
-def send_simple_message(to, subject, body):
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
         f'https://api.mailgun.net/v3/{DOMAIN}/messages',
         auth=("api", os.getenv("MAILGUN_API_KEY")),
         data={"from": f"Data Admin <mailgun@{DOMAIN}>",
               "to": [to],
               "subject": [subject],
-              "text": body})
+              "text": body,
+              "html": html
+              }
+    )
 
 
 def send_user_registration_email(email, username):
@@ -27,4 +38,5 @@ def send_user_registration_email(email, username):
         email,
         "Successfully signed up",
         f'Hi {username}! you have successfully signed up for the Chement REST API.',
+        render_template("email/action.html", username=username)
     )
